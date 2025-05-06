56ac0dadc08583843a9513fb154729345257570a
deepshikha <sdeepshikha2060@gmail.com>
2025-05-06 19:37:04 +0545

    Made a generic repository

---
+++ GroupApi/GenericClasses/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace GroupApi.GenericClasses
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> Table { get; }
+        IQueryable<TEntity> TableNoTracking { get; }
+        Task<TEntity> AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);
+        void Delete(TEntity entity);
+        void DeleteRange(IEnumerable<TEntity> entities);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+        Task<TEntity> GetByIdAsync(Guid id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+
+
+    }
+}
