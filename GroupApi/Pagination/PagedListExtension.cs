970a1ddc9a36c35adb000847f806967d3bb61e8e
deepshikha <sdeepshikha2060@gmail.com>
2025-05-06 22:49:22 +0545

    paginations

---
+++ GroupApi/Pagination/PagedListExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace GroupApi.Pagination
+{
+    public static class PagedListExtension
+    {
+        public static async Task<PaginatedList<T>> ToPagedListAsync<T>(this IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            return await new PaginatedList<T>().CreateAsync(source, pageNumber, pageSize);
+        }
+    }
+}
