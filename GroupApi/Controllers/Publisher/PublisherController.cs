c6b529758137285193c1560c062d62dbf0c34b3f
deepshikha <sdeepshikha2060@gmail.com>
2025-05-06 22:11:49 +0545

    publisher controller

--- GroupApi/Controllers/Publisher/PublisherController.cs
+++ GroupApi/Controllers/Publisher/PublisherController.cs
@@ -1,6 +1,57 @@
-﻿namespace GroupApi.Controllers.Publisher
+﻿using GroupApi.DTOs.Publisher;
+using GroupApi.Services.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupApi.Controllers.Publisher
 {
-    public class PublisherController
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class PublisherController : ControllerBase
     {
+        private readonly IPublisherService _publisherService;
+
+        public PublisherController(IPublisherService publisherService)
+        {
+            _publisherService = publisherService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _publisherService.GetAllAsync();
+            return result.IsSuccess ? Ok(result) : StatusCode((int)result.Error!.StatusCode, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await _publisherService.GetByIdAsync(id);
+            return result.IsSuccess ? Ok(result) : StatusCode((int)result.Error!.StatusCode, result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] PublisherCreateDto dto)
+        {
+            var result = await _publisherService.CreateAsync(dto);
+            return result.IsSuccess ? CreatedAtAction(nameof(GetById), new { id = result.Data!.PublisherId }, result)
+                                    : StatusCode((int)result.Error!.StatusCode, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] PublisherUpdateDto dto)
+        {
+            var result = await _publisherService.UpdateAsync(id, dto);
+            return result.IsSuccess ? Ok(result) : StatusCode((int)result.Error!.StatusCode, result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await _publisherService.DeleteAsync(id);
+            return result.IsSuccess ? NoContent() : StatusCode((int)result.Error!.StatusCode, result);
+        }
     }
+
 }
