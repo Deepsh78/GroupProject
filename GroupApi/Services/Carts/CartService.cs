92fb3fb851865d223817b9251288f2d6a455e209
deepshikha <sdeepshikha2060@gmail.com>
2025-05-10 13:40:37 +0545

    Cart Feature

--- GroupApi/Services/Carts/CartService.cs
+++ GroupApi/Services/Carts/CartService.cs
@@ -1,43 +1,180 @@
-﻿using Acb.Core.Domain;
-using GroupApi.CommonDomain;
-using GroupApi.Entities.Books;
+﻿using GroupApi.CommonDomain;
+using GroupApi.DTOs.Carts;
 using GroupApi.Entities;
 using GroupApi.GenericClasses;
-using GroupApi.Services.CurrentUser;
-using System.Net;
 using Microsoft.EntityFrameworkCore;
-using GroupApi.DTOs.Carts;
+using System.Net;
 
 namespace GroupApi.Services.Carts
 {
     public class CartService : ICartService
     {
-        public Task<GenericResponse<CartDto>> AddToCartAsync(Guid bookId, int quantity)
+        private readonly IGenericRepository<Cart> _cartRepo;
+        private readonly IGenericRepository<CartItem> _cartItemRepo;
+
+        public CartService(IGenericRepository<Cart> cartRepo, IGenericRepository<CartItem> cartItemRepo)
         {
-            throw new NotImplementedException();
+            _cartRepo = cartRepo;
+            _cartItemRepo = cartItemRepo;
         }
 
-        public Task<GenericResponse<IEnumerable<CartDto>>> GetCartByMemberAsync(Guid memberId)
+        public async Task<GenericResponse<IEnumerable<CartDto>>> GetAllAsync(Guid memberId)
         {
-            throw new NotImplementedException();
+            var cart = await _cartRepo.TableNoTracking
+                .Include(c => c.CartItems)
+                .ThenInclude(ci => ci.Book)
+                .FirstOrDefaultAsync(c => c.MemberId == memberId);
+
+            if (cart == null)
+                return new ErrorModel(HttpStatusCode.NotFound, "Book not found");
+
+            var result = cart.CartItems.Select(ci => new CartDto
+            {
+            
+                CartId = ci.CartId,
+
+            }).ToList();
+
+            return (result);
         }
 
-        public Task<GenericResponse<IEnumerable<CartDto>>> GetCartForCurrentUserAsync()
+        public async Task<GenericResponse<CartDto?>> GetByIdAsync(Guid memberId)
         {
-            throw new NotImplementedException();
+            var cart = await _cartRepo.TableNoTracking
+                .Include(c => c.CartItems)
+                .ThenInclude(ci => ci.Book)
+                .FirstOrDefaultAsync(c => c.MemberId == memberId);
+
+            if (cart == null)
+                return new ErrorModel(HttpStatusCode.NotFound, "Book not found");
+
+            var cartDto = new CartDto
+            {
+                CartId = cart.CartId,
+                MemberId = cart.MemberId,
+                CartItems = cart.CartItems.Select(ci => new CartItemDto
+                {
+                    CartItemId = ci.CartItemId,
+                    CartId = ci.CartId,
+                    BookId = ci.BookId,
+                    BookName = ci.Book.BookName,
+                    Quantity = ci.Quantity,
+                    Price = ci.Book.Price
+                }).ToList()
+            };
+
+            return (cartDto);
         }
 
-        public Task<GenericResponse<CartDto>> RemoveFromCartAsync(Guid cartItemId)
+        public async Task<GenericResponse<CartDto>> AddAsync(Guid memberId, Guid bookId, int quantity)
         {
-            throw new NotImplementedException();
+            var book = await _cartItemRepo.TableNoTracking
+                .FirstOrDefaultAsync(b => b.BookId == bookId);
+
+            if (book == null)
+                return new ErrorModel(HttpStatusCode.NotFound, "Book not found");
+
+            var cart = await _cartRepo.TableNoTracking
+                .FirstOrDefaultAsync(c => c.MemberId == memberId);
+
+            if (cart == null)
+            {
+                cart = new Cart
+                {
+                    CartId = Guid.NewGuid(),
+                    MemberId = memberId
+                };
+                await _cartRepo.AddAsync(cart);
+            }
+
+            var existingCartItem = await _cartItemRepo.TableNoTracking
+                .FirstOrDefaultAsync(ci => ci.CartId == cart.CartId && ci.BookId == bookId);
+
+            if (existingCartItem != null)
+            {
+                existingCartItem.Quantity += quantity;
+                _cartItemRepo.Update(existingCartItem);
+            }
+            else
+            {
+                var cartItem = new CartItem
+                {
+                    CartItemId = Guid.NewGuid(),
+                    CartId = cart.CartId,
+                    BookId = bookId,
+                    Quantity = quantity
+                };
+                await _cartItemRepo.AddAsync(cartItem);
+            }
+
+            await _cartRepo.SaveChangesAsync();
+
+            var cartDto = new CartDto
+            {
+                CartId = cart.CartId,
+                MemberId = cart.MemberId,
+                CartItems = cart.CartItems.Select(ci => new CartItemDto
+                {
+                    CartItemId = ci.CartItemId,
+                    CartId = ci.CartId,
+                    BookId = ci.BookId,
+                    BookName = ci.Book.BookName,
+                    Quantity = ci.Quantity,
+                    Price = ci.Book.Price
+                }).ToList()
+            };
+
+            return (cartDto);
         }
 
-        public Task<GenericResponse<CartDto>> UpdateCartItemAsync(Guid cartItemId, int quantity)
+        public async Task<GenericResponse<CartDto>> UpdateAsync(Guid cartItemId, int quantity)
         {
-            throw new NotImplementedException();
+            var cartItem = await _cartItemRepo.GetByIdAsync(cartItemId);
+            if (cartItem == null)
+                return new ErrorModel(HttpStatusCode.NotFound, "Items not found");
+
+            cartItem.Quantity = quantity;
+            _cartItemRepo.Update(cartItem);
+            await _cartRepo.SaveChangesAsync();
+
+            var cartDto = new CartDto
+            {
+                CartId = cartItem.CartId,
+                MemberId = cartItem.Cart.MemberId,
+                CartItems = new List<CartItemDto>
+                {
+                    new CartItemDto
+                    {
+                        CartItemId = cartItem.CartItemId,
+                        CartId = cartItem.CartId,
+                        BookId = cartItem.BookId,
+                        BookName = cartItem.Book.BookName,
+                        Quantity = cartItem.Quantity,
+                        Price = cartItem.Book.Price
+                    }
+                }
+            };
+
+            return (cartDto);
+        }
+
+        public async Task<GenericResponse<CartDto>> RemoveAsync(Guid cartItemId)
+        {
+            var cartItem = await _cartItemRepo.GetByIdAsync(cartItemId);
+            if (cartItem == null)
+                return new ErrorModel(HttpStatusCode.NotFound, "Book not found");
+
+            _cartItemRepo.Delete(cartItem);
+            await _cartRepo.SaveChangesAsync();
+
+            var cartDto = new CartDto
+            {
+                CartId = cartItem.CartId,
+                MemberId = cartItem.Cart.MemberId,
+                CartItems = new List<CartItemDto>()
+            };
+
+            return (cartDto);
         }
     }
-
-
-
 }
--- GroupApi/Services/Carts/ICartService.cs
+++ GroupApi/Services/Carts/ICartService.cs
@@ -5,11 +5,20 @@ namespace GroupApi.Services.Carts
 {
     public interface ICartService
     {
-        Task<GenericResponse<IEnumerable<CartDto>>> GetCartByMemberAsync(Guid memberId);
-        Task<GenericResponse<CartDto>> AddToCartAsync(Guid bookId, int quantity);
-        Task<GenericResponse<CartDto>> UpdateCartItemAsync(Guid cartItemId, int quantity);
-        Task<GenericResponse<CartDto>> RemoveFromCartAsync(Guid cartItemId);
-        Task<GenericResponse<IEnumerable<CartDto>>> GetCartForCurrentUserAsync();
+        // Get all items in the cart for a specific member
+        Task<GenericResponse<IEnumerable<CartDto>>> GetAllAsync(Guid memberId);
+
+        // Get a specific cart by member ID
+        Task<GenericResponse<CartDto?>> GetByIdAsync(Guid memberId);
+
+        // Add a book to the cart
+        Task<GenericResponse<CartDto>> AddAsync(Guid memberId, Guid bookId, int quantity);
+
+        // Update the quantity of a book in the cart
+        Task<GenericResponse<CartDto>> UpdateAsync(Guid cartItemId, int quantity);
+
+        // Remove a book from the cart
+        Task<GenericResponse<CartDto>> RemoveAsync(Guid cartItemId);
     }
 
 }
